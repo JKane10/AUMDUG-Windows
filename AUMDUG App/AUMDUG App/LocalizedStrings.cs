911087e7f6ed8e4211f171fa241952727d1e21c7
Josh Kane <jgkane2119@yahoo.com>
2013-11-18 21:53:49 -0600

    Initial Commit
    Initial Commit, contains nothing fancy. Just the standard pivot template
    with the headers we need.

---
+++ AUMDUG App/AUMDUG App/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AUMDUG_App.Resources;
+
+namespace AUMDUG_App
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
